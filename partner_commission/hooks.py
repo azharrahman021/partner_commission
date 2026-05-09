8f1f94f9abdd99b4133e08463c0da0642f7c1280
Azhar Rahman <azharrahman021@gmail.com>
2026-05-09 07:49:31 +0000

    feat: Initialize App

--- partner_commission/hooks.py
+++ partner_commission/hooks.py
@@ -1,7 +1,7 @@
 app_name = "partner_commission"
 app_title = "Partner Commission"
-app_publisher = "Azhar"
-app_description = "sales commission management"
+app_publisher = "azhar"
+app_description = "sales commission"
 app_email = "azharrahman021@gmail.com"
 app_license = "mit"
 
